f342c74383e921e90503c14fbcb41f99fd4bb786
Miroslav Exner <exac@exac.info>
2022-05-07 21:04:39 +0200

    Adds initial files from Springboot initializer

---
+++ src/main/java/info/exac/bluelife/BluelifeApplication.java
@@ -0,0 +1,27 @@
+package info.exac.bluelife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+@SpringBootApplication
+@EnableWebMvc
+public class BlueLifeApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueLifeApplication.class, args);
+    }
+
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+    }
+
+}
---
+++ src/test/java/info/exac/bluelife/BluelifeApplicationTests.java
@@ -0,0 +1,14 @@
+package info.exac.bluelife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluelifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
